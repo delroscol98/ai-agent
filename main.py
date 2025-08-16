e4c1a4dede61ecc75f7e9ee9c58a93c48fe56e40
delroscol98 <collindelrosario98@gmail.com>
2025-08-16 22:18:41 +1000

    refactor: set contents to sys.argv[1] to accept prompts from the command line

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import os
+import sys
 
 from dotenv import load_dotenv
 from google import genai
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-client = genai.Client(api_key=api_key)
-response = client.models.generate_content(
-    model="gemini-2.0-flash-001",
-    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+try:
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=sys.argv[1]
     )
-print(response.text, response.usage_metadata.prompt_token_count, response.usage_metadata.candidates_token_count)
+except Exception as e:
+    print(e)
+    sys.exit(1)
