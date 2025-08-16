113fdab5e148530ab7c4d06b74ae49d2f165c973
delroscol98 <collindelrosario98@gmail.com>
2025-08-16 21:43:29 +1000

    feature: imported dotenv to read environment variable in .env. imported google to interact with Gemini.

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-def main():
-    print("Hello from ai-agent!")
+import os
 
+from dotenv import load_dotenv
+from google import genai
 
-if __name__ == "__main__":
-    main()
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
+response = client.models.generate_content(
+    model="gemini-2.0-flash-001",
+    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    )
+print(response.text, response.usage_metadata.prompt_token_count, response.usage_metadata.candidates_token_count)
