16f02ab02477d73d7f37fd29f7a82f99c87960de
delroscol98 <collindelrosario98@gmail.com>
2025-08-17 06:45:44 +1000

    feature: users can add the --verbose flag and receive different outputs with the response text, either with the prompt and tokens or not.

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from google import genai
 from google.genai import types
 
 
-def generate_content(client, messages):
+def generate_content(client, messages, verbose):
     # response from the gemini-2.0-flash-001 model
     text_response = client.models.generate_content(
         model="gemini-2.0-flash-001",
@@ -19,9 +19,12 @@ def generate_content(client, messages):
     # Number of tokens in the response
     response_tokens = text_response.usage_metadata.candidates_token_count
 
+    if verbose:
+        print(f"Prompt tokens: {prompt_tokens}")
+        print(f"Response tokens: {response_tokens}")
+
+    print("---------------AI Response---------------")
     print(text_response.text)
-    print(prompt_tokens)
-    print(response_tokens)
 
 
 def main():
@@ -30,17 +33,25 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
-    args = sys.argv[1:]
-
     # Check for user_prompt
+    args = []
+    for arg in sys.argv:
+        if not arg.startswith("--"):
+            args.append(arg)
+
     if not args:
-        print("Your personal AI Assistant")
-        print('\nUsage: python main.py "your prompt here"')
+        print("---------------Gemini AI Assistant---------------")
+        print('\nUsage: python main.py "your prompt here" [--verbose]')
         print('Example: python main.py "How do I build a calculator app?"')
         sys.exit(1)
 
-    # retrieves user prompt
-    user_prompt = args[0]
+    # retrieves user prompt and checks for "--verbose" flag
+    user_prompt = " ".join(args)
+    verbose = "--verbose" in sys.argv
+
+    if verbose:
+        print("---------------User Prompt---------------")
+        print(f"\nUser prompts: {user_prompt}")
 
     # stores a list of messages for Gemini to see the
     # whole conversation and answer with the whole context
@@ -48,7 +59,8 @@ def main():
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
 
-    generate_content(client, messages)
+    # Generates content from Gemini AI
+    generate_content(client, messages, verbose)
 
 
 if __name__ == "__main__":
