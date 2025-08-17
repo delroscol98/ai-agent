7c3e43f0f7867350166e6fd29373812bc5c2d543
delroscol98 <collindelrosario98@gmail.com>
2025-08-17 22:33:55 +1000

    feature: created calculator app with tests and a package library to import into main.py

---
+++ calculator/pkg/calculator.py
@@ -0,0 +1,68 @@
+class Calculator:
+    def __init__(self):
+        self.operators = {
+            "+": lambda a, b: a + b,
+            "-": lambda a, b: a - b,
+            "*": lambda a, b: a * b,
+            "/": lambda a, b: a / b,
+        }
+
+        self.precedence = {
+            "+": 1,
+            "-": 1,
+            "*": 2,
+            "/": 2,
+        }
+
+    def evaluate(self, expression):
+        if not expression or expression.isspace():
+            return None
+        tokens = expression.strip().split()
+        return self._evaluate_infix(tokens)
+
+    def _evaluate_infix(self, tokens):
+        # inititalise stack for numerical values
+        values = []
+        # initialise stack for operands
+        operators = []
+
+        # loop through tokens to compute expression on the fly
+        # if token is a number append it to the values stack
+        # if token is operand, check 1) if operators if non-empty; 2) if the last token in the operators stack is in the operators object; 3) if the previous token in the operators stack has higher precedence than current token
+        for token in tokens:
+            if token in self.operators:
+                while (
+                    operators
+                    and operators[-1] in self.operators
+                    and self.precedence[operators[-1]] >= self.precedence[token]
+                ):
+                    self._apply_operator(operators, values)
+                operators.append(token)
+            else:
+                try:
+                    values.append(float(token))
+                except ValueError:
+                    raise ValueError(f"invalid token: {token}")
+
+        while operators:
+            self._apply_operator(operators, values)
+
+        if len(values) != 1:
+            raise ValueError("invalid expression")
+
+        return values[0]
+
+    # computes the last 2 numerical values with the last operand
+    def _apply_operator(self, operators, values):
+        if not operators:
+            return
+
+        operator = operators.pop()
+        if len(values) < 2:
+            raise ValueError(f"not enough operands for operator {operator}")
+
+        b = values.pop()
+        a = values.pop()
+
+        # appends the computed result to the values stack
+        values.append(self.operators[operator](a, b))
