3a80384e47c153adda257cb1968528af54db6a0d
delroscol98 <collindelrosario98@gmail.com>
2025-08-19 05:40:24 +1000

    feature: created a function that reads the contents of a file

---
+++ functions/get_files_content.py
@@ -0,0 +1,25 @@
+import os
+from config.config import MAX_CHARS
+
+
+def get_files_content(working_directory, file_path):
+    abs_working_path = os.path.abspath(working_directory)
+    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
+
+    if not abs_file_path.startswith(abs_working_path):
+        return (
+            f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+        )
+
+    if not os.path.isfile(abs_file_path):
+            return f'Error: File not found or is not a regular file: "{file_path}"'
+
+    try:
+        with open(abs_file_path, "r") as f:
+            file_content_string = f.read(MAX_CHARS)
+            file_size = os.path.getsize(abs_file_path)
+            if file_size > MAX_CHARS:
+                file_content_string += '{file_content_string} [...File "{file_path}" truncated at 10000 characters]'
+            return file_content_string
+    except Exception as e:
+        return f"Error: {e}"
