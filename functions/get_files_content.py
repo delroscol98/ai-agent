365c1a0943f22085da1bdab20f8b3ccb24b994a1
delroscol98 <collindelrosario98@gmail.com>
2025-08-19 19:22:08 +1000

    docs: updated with comments

--- functions/get_files_content.py
+++ functions/get_files_content.py
@@ -3,18 +3,22 @@ from config.config import MAX_CHARS
 
 
 def get_files_content(working_directory, file_path):
+    # get absolute paths to file_path
     abs_working_path = os.path.abspath(working_directory)
     abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
 
+    # check if the file_path is outside the working_directory
     if not abs_file_path.startswith(abs_working_path):
         return (
             f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
         )
 
+    # check if the path leads to a file
     if not os.path.isfile(abs_file_path):
             return f'Error: File not found or is not a regular file: "{file_path}"'
 
     try:
+        # read the file up to a MAX_CHARS length
         with open(abs_file_path, "r") as f:
             file_content_string = f.read(MAX_CHARS)
             file_size = os.path.getsize(abs_file_path)
