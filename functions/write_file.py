33848b8f51f0010763a012bd3e6cb4d6cc6b940e
delroscol98 <collindelrosario98@gmail.com>
2025-08-19 10:35:42 +1000

    refacotor: broke the try-except statement to check each operation 1) if the path leads to a directory 2) writing to the file

--- functions/write_file.py
+++ functions/write_file.py
@@ -8,11 +8,19 @@ def write_file(working_directory, file_path, content):
     if not abs_file_path.startswith(abs_working_dir_path):
         return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
 
+    if not os.path.exists(abs_file_path):
+        try:
+            os.makedirs(os.path.dirname(abs_file_path))
+        except Exception as e:
+            return f"Error: Creating directory: {e}"
+
+    else:
+        if os.path.isdir(abs_file_path):
+            return f"Error: {abs_file_path} is a directory not a file"
+
     try:
-        if not os.path.exists(os.path.dirname(abs_file_path)):
-            os.makedirs(abs_file_path)
         with open(abs_file_path, "w") as f:
             f.write(content)
-            return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+        return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
     except Exception as e:
         return f"Error: {e}"
