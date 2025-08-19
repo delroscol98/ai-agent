69137019f8f6b43878292c022fc120278631ccda
delroscol98 <collindelrosario98@gmail.com>
2025-08-19 10:19:16 +1000

    test: updated tests for write_file function and created txt files from test

--- tests.py
+++ tests.py
@@ -1,22 +1,16 @@
-from functions.get_files_content import get_files_content
+from functions.write_file import write_file
 
 
 def test():
-    result = get_files_content("calculator", "main.py")
-    print("Result for current main.py:")
+    result = write_file("calculator", "lorem.txt", "wait, this ins't lorem ipsum")
     print(result)
 
-    result = get_files_content("calculator", "pkg/calculator.py")
-    print("Result for pkg/calculator.py:")
+    result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
     print(result)
 
-    result = get_files_content("calculator", "/bin/cat")
-    print("Result for /bin/cat:")
+    result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
     print(result)
 
-    result = get_files_content("calculator", "pkg/does_not_exist.py")
-    print("Result for pkg/does_not_exist.py:")
-    print(result)
 
 if __name__ == "__main__":
     test()
